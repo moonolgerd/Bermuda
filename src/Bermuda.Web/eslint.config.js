1ed88a575b25197e25d2d31b1306a6b31d4f8e4c
Oleg <moonolgerd@gmail.com>
2026-07-10 18:51:14 -0400

    chore: add vitest reference to vite config and create aspire configuration file

--- src/Bermuda.Web/eslint.config.js
+++ src/Bermuda.Web/eslint.config.js
@@ -2,28 +2,43 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import babelParser from '@babel/eslint-parser'
 
-export default tseslint.config(
+// NOTE: typescript-eslint does not yet support TypeScript 7's rewritten
+// compiler internals (crashes on import - see @typescript-eslint/typescript-estree
+// create-program/shared.js referencing removed `ts.Extension` APIs). Until it
+// is updated, TS/TSX files are parsed with @babel/eslint-parser (syntax-only,
+// no type-aware rules). Switch back to typescript-eslint once it publishes a
+// release supporting TypeScript 7.
+export default [
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-typescript', '@babel/preset-react'],
+        },
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       'semi': ['error', 'never'],
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
     },
   },
-)
+]
